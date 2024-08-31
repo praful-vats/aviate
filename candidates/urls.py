5b5f380712cd296923e141cf408e86be310cfad0
prafulvats <prafulvats01@gmail.com>
2024-08-31 13:45:56 +0530

    name based search

--- candidates/urls.py
+++ candidates/urls.py
@@ -5,4 +5,5 @@ from .views import create_candidate, CandidateDetailView
 urlpatterns = [
     path('<int:pk>/', CandidateDetailView.as_view(), name='candidate-detail'),
     path('create/', create_candidate, name='create-candidate'),
+    path('webhook/', views.typeform_webhook, name='typeform-webhook'),
 ]
--- candidates/views.py
+++ candidates/views.py
@@ -6,6 +6,9 @@ from .tasks import process_candidate_data
 from django.contrib import messages
 from .forms import CandidateForm
 import logging
+import json
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
 logger = logging.getLogger(__name__)
 
@@ -27,3 +30,38 @@ def create_candidate(request):
         form = CandidateForm()
     
     return render(request, 'candidates/create_candidate.html', {'form': form})
+
+@csrf_exempt
+def typeform_webhook(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+
+        form_responses = data.get('form_response', {}).get('answers', [])
+        
+        candidate_data = {}
+        for response in form_responses:
+            field_id = response.get('field', {}).get('id')
+            answer = response.get('text') or response.get('number')
+
+            if field_id == 'your_field_id_for_name':
+                candidate_data['name'] = answer
+            elif field_id == 'your_field_id_for_age':
+                candidate_data['age'] = answer
+            elif field_id == 'your_field_id_for_gender':
+                candidate_data['gender'] = answer
+            elif field_id == 'your_field_id_for_years_of_exp':
+                candidate_data['years_of_experience'] = answer
+            elif field_id == 'your_field_id_for_phone_number':
+                candidate_data['phone_number'] = answer
+            elif field_id == 'your_field_id_for_email':
+                candidate_data['email'] = answer
+            elif field_id == 'your_field_id_for_current_salary':
+                candidate_data['current_salary'] = answer
+            elif field_id == 'your_field_id_for_expected_salary':
+                candidate_data['expected_salary'] = answer
+
+        Candidate.objects.create(**candidate_data)
+
+        return JsonResponse({'status': 'success'})
+
+    return JsonResponse({'status': 'invalid request'}, status=400)
